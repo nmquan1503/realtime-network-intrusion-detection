02ab1117f28f21212d66b16ee75e66cb20a34de4
VanDaiUet <23020036@gmail.com>
2026-06-04 04:56:01 +0700

    Update build images script

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,9 +1,13 @@
 docker build -f docker/Dockerfile.batch -t mquan1503/bigdata-batch:latest .
 docker build -f docker/Dockerfile.simulator -t mquan1503/bigdata-simulator:latest .
+docker build -f docker/Dockerfile.streaming -t mquan1503/bigdata-streaming:latest .
+docker build -f docker/Dockerfile.dashboard -t mquan1503/bigdata-dashboard:latest .
 # docker build -f docker/Dockerfile.batch -t mquan1503/bigdata-spark:latest .
 docker build -f docker/Dockerfile.airflow -t mquan1503/bigdata-airflow:latest .
 
 docker push mquan1503/bigdata-batch:latest
 docker push mquan1503/bigdata-simulator:latest
+docker push mquan1503/bigdata-streaming:latest
+docker push mquan1503/bigdata-dashboard:latest
 # docker push mquan1503/bigdata-spark:latest
 docker push mquan1503/bigdata-airflow:latest
